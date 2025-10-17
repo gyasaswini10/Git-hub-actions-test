911388acbbb660af84878af779e2017879b49d47
GURIVI REDDY YASASWINI <gyasu1110@gmail.com>
2025-10-17 21:27:53 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Hospital-Lab-Exam/', 
+  base: 'Git-hub-actions-test', 
   plugins: [react()],
 })
